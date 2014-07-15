ad85aab86c7763351328711871496a13c735761d
Tim Rogers <tiroger@microsoft.com>
2014-07-15 14:06:14 -0700

    parting out the wsman setup to its own class

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,18 +13,7 @@ class cloudbase_prep {
     require => Class['openstack_hyper_v::openstack::folders'],
   }
 
-  windows_common::remote_file{'CBS_prep_WSMan_script':
-# updated on 4-24-2014, per Octavian
-#    source      => 'https://raw2.github.com/cloudbase/unattended-setup-scripts/master/SetupWinRMAccess.ps1',
-    source      => 'https://raw.githubusercontent.com/cloudbase/unattended-setup-scripts/master/SetupWinRMAccessSelfSigned.ps1',
-    destination => 'c:/SetupWinRMAccess.ps1',
-  }
-  exec {'CBS_prep_WSMan':
-    command => "cmd.exe /c powershell -executionpolicy Unrestricted -File c:/SetupWinRMAccess.ps1",
-    path    => "${systemdrive}\\windows\\system32;${systemdrive}\\windows\\system32\\WindowsPowerShell\\v1.0",
-	creates => "c:/OpenSSL-Win32/CA/certs/cert.pfx",
-	require => Windows_common::Remote_file['CBS_prep_WSMan_script'],
-  }
+  class { 'cloudbase_prep::wsman': }
 
   vcsrepo {'cloudbase_scripts':
     ensure      => 'latest',
---
+++ manifests/wsman.pp
@@ -0,0 +1,14 @@
+class cloudbase_prep::wsman {
+  windows_common::remote_file{'CBS_prep_WSMan_script':
+# updated on 4-24-2014, per Octavian
+#    source      => 'https://raw2.github.com/cloudbase/unattended-setup-scripts/master/SetupWinRMAccess.ps1',
+    source      => 'https://raw.githubusercontent.com/cloudbase/unattended-setup-scripts/master/SetupWinRMAccessSelfSigned.ps1',
+    destination => 'c:/SetupWinRMAccess.ps1',
+  }
+  exec {'CBS_prep_WSMan':
+    command => "cmd.exe /c powershell -executionpolicy Unrestricted -File c:/SetupWinRMAccess.ps1",
+    path    => "${systemdrive}\\windows\\system32;${systemdrive}\\windows\\system32\\WindowsPowerShell\\v1.0",
+	creates => "c:/OpenSSL-Win32/CA/certs/cert.pfx",
+	require => Windows_common::Remote_file['CBS_prep_WSMan_script'],
+  }
+}
